016f3190cd6dd7d1b49971dc3bcca985bc0d6f35
Isabel Henrique <contato.isabelhenrique@gmail.com>
2026-07-10 11:45:38 -0300

    middleware inertia

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,6 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->preventRequestForgery(except: [
             'categories/*/import*'
+        ]);
+
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
         ]);
 
         $middleware->alias([
