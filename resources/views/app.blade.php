b6e07a3dfe1d8143a00a05025de76fbfaa150641
Isabel Henrique <contato.isabelhenrique@gmail.com>
2026-07-16 14:56:01 -0300

    fix: evitar tela branca no carregamento

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-BR" class="dark" style="background-color: #0b0f19;">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,7 +7,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     <x-inertia::head />
 </head>
-<body class="bg-gray-50 font-sans text-gray-900 antialiased min-h-screen">
+<body class="font-sans antialiased min-h-screen text-[#e2e8f0]" style="background-color: #0b0f19;">
 <x-inertia::app />
 </body>
 </html>
